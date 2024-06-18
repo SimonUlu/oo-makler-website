abe73826d67cd2b959aee82dbe25b3a5fddffdae
Simon Unterlugauer <simonunterlugauer@Laptop-von-Simon.local>
2024-06-18 17:54:13 +0200

    finishing home page

--- resources/views/pages/estate/estate-columns-3-ref.blade.php
+++ resources/views/pages/estate/estate-columns-3-ref.blade.php
@@ -2,41 +2,45 @@
 <div x-data="slider()" class="relative mt-8">
     <div class="w-full grid grid-cols-3 lg:gap-6 lg:mt-6 lg:space-y-0 lg:space-x-2 space-y-10">
         
-        <template x-for="(estate, index) in estates" :key="estate.id">
+        <template x-for="(estate, index) in estates" :key="estate.elements.id">
+
             <li 
                 class="flex flex-col items-center justify-center w-full p-2 shrink-0 snap-start" 
                 role="option" 
                 x-show="activeSlides.includes(Number(index))"
             >
-                <div
-                    x-data="{ hover: false }"
-                    @mouseover="hover = true"
-                    @mouseout="hover = false"
-                    class="bg-cover bg-no-repeat bg-center w-full h-[28rem] relative cursor-pointer"
-                    :style="'background-image: url(' + estate.elements.images[0].url + ')'"
-                >
-                    <a target="_blank">
-                        <div
-                            :class="{ 'h-1/3': !hover, 'h-3/4': hover }"
-                            class="transition-all w-full bg-primary-800 flex flex-col justify-end p-4 absolute bottom-0 overflow-hidden"
-                            :style="{'background-color': hover ? 'rgba(0,103,177, 0.8)' : 'rgba(0,103,177, 0.6)'}"
-                        >
-                            <div
-                                :class="{ 'translate-y-0': !hover, '-translate-y-1/4': hover }"
-                                class="transition-transform"
-                            >
-                                <img
-                                    x-show="hover"
-                                    src="/logo_images/logo_white.png"
-                                    class="h-12 mr-3 pb-4"
-                                    alt="Test"
-                                />
-                                <h3 class="text-white font-bold text-xl" x-text="estate.elements.objekttitel"></h3>
-                                <p class="text-white text-md" x-text="estate.elements.plz + ' ' + estate.elements.plz"></p>
-                                <p x-show="hover" class="text-white text-sm pt-2" x-text="estate.elements.objektbeschreibung.substring(0, 150)"></p>
+                <div  class="relative w-full" data-carousel="static">
+                    <!-- Carousel wrapper -->
+                    <div class="relative w-auto overflow-hidden  h-72 min-w-64">
+                        <div>
+                            <div class="absolute inset-0 transition-transform transform translate-x-0 z-20">
+                                <img 
+                                    x-ref="img" 
+                                    class="object-cover w-full h-full" 
+                                    alt="1a LAGE und Renditeobjekt!!!!!" 
+                                    :src="estate.elements.images[0].url"
+                                >
                             </div>
                         </div>
-                    </a>
+                        
+                        
+                    </div>
+                </div>
+                <div class="relative px-5 pt-2 pb-2 border w-full  md:pt-0 flex">      
+                    <div class="flex-2">              
+                        <h3 class="flex justify-between text-lg font-bold text-gray-900 mt-8">
+                            <span class="capitalize mb-2 text-primary" x-text="estate.elements.objektart">
+                            </span>
+                        </h3>
+                        <div class="space-y-2 text-md mb-4">
+                            <div class="flex space-x-2 text-primary text-base" x-text="estate.elements.plz + ' ' +estate.elements.ort"></div>
+                        </div>
+                    </div>
+                    <div class="flex-1 flex items-center justify-end">
+                        <span class="bg-secondary text-white py-1 px-5 text-md">
+                            verkauft
+                        </span>   
+                    </div>
                 </div>
             </li>
         </template>
@@ -44,9 +48,9 @@
     <div class="flex justify-center space-x-2 mt-4">
         <template x-for="(page, pageIndex) in Array(Math.ceil(Object.keys(estates).length / 3)).fill()" :key="pageIndex">
             <button 
-                x-on:click="activeSlides = [pageIndex * 3, pageIndex * 3 + 1, pageIndex * 3 + 2]" 
+                x-on:click="activeSlides = [pageIndex * 3, pageIndex * 3 + 1, pageIndex * 3 + 2], logEstates()" 
                 :class="{'bg-primary': activeSlides[0] === pageIndex * 3, 'bg-white': activeSlides[0] !== pageIndex * 3}" 
-                class="w-4 h-4 rounded-full focus:outline-none"
+                class="w-4 h-4 rounded-full focus:outline-none border border-gray-500"
             ></button>
         </template>
     </div>
@@ -66,6 +70,9 @@ function slider() {
                     (slide + 3)
                 );
             }
+        },
+        logEstates() {
+            console.log(this.estates);
         },
         prev() {
 
